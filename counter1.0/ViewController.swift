84836fdc6846efc0409dc514672e16966e6e819b
20161104574 <a20161104574@admindeimac-12.local>
2018-10-24 10:42:20 +0800

    Initial Commit

---
+++ counter1.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  counter1.0
+//
+//  Created by 20161104574 on 2018/10/24.
+//  Copyright © 2018年 20161104574. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
