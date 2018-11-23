9496e27716a1fd1d6c8be2f6ffa716d64302b891
20161104574 <a20161104574@admindeiMac-12.local>
2018-11-23 16:59:02 +0800

    CJFHKHG
    FIKYUGIUYI

--- counter1.0/ViewController.swift
+++ counter1.0/ViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+    var re = 0 //判断result.text前是否存在符号
+    var judge = 0//决定输出数字的位数
     var number = 0
     var a = 0.0
     var b = 0.0
@@ -21,7 +22,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var result: UILabel!
     
     @IBAction func number0(_ sender: Any) {
-        if result.text == ""
+        if re == 1
         {
             result.text = "0"
         }
@@ -32,56 +33,125 @@ class ViewController: UIViewController {
     }
     
     @IBAction func number1(_ sender: Any) {
-        result.text = result.text! + "1"
-    }
-    
-    @IBAction func number2(_ sender: Any) {
-        result.text = result.text! + "2"
-    }
-    
-    @IBAction func number3(_ sender: Any) {
-        result.text = result.text! + "3"
-    }
-    
-    @IBAction func number4(_ sender: Any) {
-        result.text = result.text! + "4"
-    }
-    
-    @IBAction func number5(_ sender: Any) {
-        result.text = result.text! + "5"
-    }
-    
-    @IBAction func number6(_ sender: Any) {
-        result.text = result.text! + "6"
-    }
-    
-    @IBAction func number7(_ sender: Any) {
-        result.text = result.text! + "7"
-    }
-    
-    @IBAction func number8(_ sender: Any) {
-        result.text = result.text! + "8"
-    }
-    
-    @IBAction func number9(_ sender: Any) {
-        result.text = result.text! + "9"
-    }
-    
-    @IBAction func dian(_ sender: Any) {
-        if d == 0
+        if re == 1
         {
-            result.text = result.text! + "."
+            result.text = "1"
         }
         else
         {
-            result.text = result.text
+            result.text = result.text! + "1"
         }
+    }
+    
+    @IBAction func number2(_ sender: Any) {
+        if re == 1
+        {
+            result.text = "2"
+        }
+        else
+        {
+            result.text = result.text! + "2"
+        }
+    }
+    
+    @IBAction func number3(_ sender: Any) {
+        if re == 1
+        {
+            result.text = "3"
+        }
+        else
+        {
+            result.text = result.text! + "3"
+        }
+    }
+    
+    @IBAction func number4(_ sender: Any) {
+        if re == 1
+        {
+            result.text = "4"
+        }
+        else
+        {
+            result.text = result.text! + "4"
+        }
+    }
+    
+    @IBAction func number5(_ sender: Any) {
+        if re == 1
+        {
+            result.text = "5"
+        }
+        else
+        {
+            result.text = result.text! + "5"
+        }
+    }
+    
+    @IBAction func number6(_ sender: Any) {
+        if re == 1
+        {
+            result.text = "6"
+        }
+        else
+        {
+            result.text = result.text! + "6"
+        }
+    }
+    
+    @IBAction func number7(_ sender: Any) {
+        if re == 1
+        {
+            result.text = "7"
+        }
+        else
+        {
+            result.text = result.text! + "7"
+        }
+    }
+    
+    @IBAction func number8(_ sender: Any) {
+        if re == 1
+        {
+            result.text = "8"
+        }
+        else
+        {
+            result.text = result.text! + "8"
+        }
+    }
+    
+    @IBAction func number9(_ sender: Any) {
+        if re == 1
+        {
+            result.text = "9"
+        }
+        else
+        {
+            result.text = result.text! + "9"
+        }
+    }
+    
+    @IBAction func dian(_ sender: Any) {
+        result.text = result.text! + "."
+        
+        judge = 1
+        /*if re == 1
+        {
+            result.text = "."
+        }
+        else
+        {
+            result.text = result.text! + "."
+        }*/
     }
     
     @IBAction func clear(_ sender: Any) {
         result.text = ""
+        
+        re = 0
+        /*result.text = ""
         result1.text = ""
-        result2.text = ""
+        result2.text = ""*/
     }
     
     @IBAction func change(_ sender: Any) {
@@ -124,34 +194,7 @@ class ViewController: UIViewController {
         if result.text == ""
         {
             result.text = "0"
-            number = 2
-        }
-        else
-        {
-            a = Double(result.text!)!
-            result1.text = String(a)
-            result.text  = ""
-            number = 2
-            d = 0
-        }
-    }
-    
-    @IBAction func cheng(_ sender: Any) {
-        if result.text == ""
-        {
-            result.text = "0"
-            number = 3
-        }
-        else
-        {
-            a = Double(result.text!)!
-            result1.text = String(a)
-            result.text  = ""
-            number = 3
-            d = 0
-        }
-    }
-    
+            number = 2    
     @IBAction func chu(_ sender: Any) {
         if result.text == ""
         {
@@ -170,6 +213,7 @@ class ViewController: UIViewController {
     
     @IBAction func equal(_ sender: Any) {
         b = Double(result.text!)!
+        result2.text = String(b)
         result.text = "0"
         if number == 1
         {
